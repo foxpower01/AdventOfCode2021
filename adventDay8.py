4345bd50bd654d7c2a354c1c6d4b8e70d7e09eb4
foxpower01 <rin.studys@gmail.com>
2022-01-13 10:04:02 -0800

    in progress commit

--- adventDay8.py
+++ adventDay8.py
@@ -1,7 +1,20 @@
 outputs = []
+tempPuts = []
+counter = 0
 f = open("myInputDay8.txt", "r")
 for line in f:
+    print(line)
     line = line.split(" | ")
     line = list(map(lambda x: str(x), line))
-    outputs.append(line[1])
-outputs = list(map(lambda jtem: jtem.replace("\n", ""), outputs))
+    tempPuts.append(line[1])
+tempPuts = list(map(lambda jtem: jtem.replace("\n", ""), tempPuts))
+print(tempPuts)
+for item in tempPuts:
+    for i in range(4):
+        outputs.append(item.split(" ")[i])
+print(outputs)
+for jtem in outputs:
+    print(len(jtem))
+    if len(jtem) == 1 or len(jtem) == 4 or len(jtem) == 7 or len(jtem) == 8:
+        counter += 1
+print(counter)
