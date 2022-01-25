b408478d283ea179ba68df8c32d0e38880679f87
foxpower01 <rin.studys@gmail.com>
2022-01-25 10:05:06 -0800

    in progress

--- adventDay8part2.py
+++ adventDay8part2.py
@@ -1,6 +1,8 @@
 outputs = []
 tempPuts = []
+posA = posB = posC = posD = posE = posF = posG = 0
 counter = 0
+tempJtem = []
 f = open("myInputDay8.txt", "r")
 for line in f:
     print(line)
@@ -14,8 +16,12 @@ for item in tempPuts:
     for i in range(4):
         outputs.append(item.split(" ")[i])
 print(outputs)
+#for jtem in outputs:
+#    print(len(jtem))
+#    if len(jtem) == 2 or len(jtem) == 3 or len(jtem) == 4 or len(jtem) == 7:
+#        counter += 1
+#print(counter)
 for jtem in outputs:
-    print(len(jtem))
-    if len(jtem) == 2 or len(jtem) == 3 or len(jtem) == 4 or len(jtem) == 7:
-        counter += 1
-print(counter)
+    if len(jtem) == 2:
+        tempJtem = list(jtem)
+#        print(tempJtem)
