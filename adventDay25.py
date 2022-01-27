c2105b6c8d8321f8531f819e7e3ee61d7fe99614
foxpower01 <rin.studys@gmail.com>
2022-01-27 10:05:12 -0800

    initial commit

---
+++ adventDay25.py
@@ -0,0 +1,11 @@
+f = open("myInputDay25.txt", "r")
+boardstate = []
+for line in f:
+    boardstate.append(list(filter(lambda a: a != "\n", line)))
+print(boardstate)
+
+def canMoveEast(line, position):
+    if 
+
+def step(boardstate):
+    for i in range(len(boardstate)):
