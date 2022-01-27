2fded539ed5446b91476665d82b48a0379006cdc
foxpower01 <rin.studys@gmail.com>
2022-01-27 09:32:40 -0800

    in progress

--- adventDay8part2.py
+++ adventDay8part2.py
@@ -4,24 +4,48 @@ posA = posB = posC = posD = posE = posF = posG = 0
 counter = 0
 tempJtem = []
 f = open("myInputDay8.txt", "r")
+
+def isOne(item):
+    if len(item) == 2:
+        return(True)
+
+def isSeven(item):
+    if len(item) == 3:
+        return(True)
+
+def isFour(item):
+    if len(item) == 4:
+        return(True)
+
+def isEight(item):
+    if len(item) == 7:
+        return(True)
+
 for line in f:
     print(line)
     line = line.split(" | ")
     line = list(map(lambda x: str(x), line))
     print(line)
     tempPuts.append(line[1])
-tempPuts = list(map(lambda jtem: jtem.replace("\n", ""), tempPuts))
-print(tempPuts)
-for item in tempPuts:
-    for i in range(4):
-        outputs.append(item.split(" ")[i])
+temPuts = list(map(lambda jtem: jtem.replace("\n", ""), tempPuts))
+for line in tempPuts:
+    outputs.append(line.split(" "))
 print(outputs)
+#for item in tempPuts:
+#    for i in range(4):
+#        outputs.append(item.split(" ")[i])
+#print(outputs)
 #for jtem in outputs:
 #    print(len(jtem))
 #    if len(jtem) == 2 or len(jtem) == 3 or len(jtem) == 4 or len(jtem) == 7:
 #        counter += 1
 #print(counter)
-for jtem in outputs:
-    if len(jtem) == 2:
-        tempJtem = list(jtem)
-#        print(tempJtem)
+for line in outputs:
+    print(line)
+    for item in line:
+        print(item)
+        if len(item) == 2:
+            tempItem = list(item)
+            print(tempItem)
+            print("hi")
+
