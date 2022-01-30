a2d0079333a66666795e70b54bae15a985a2d8f7
foxpower01 <rin.studys@gmail.com>
2022-01-29 23:01:46 -0800

    in progress

--- adventDay25.py
+++ adventDay25.py
@@ -1,3 +1,6 @@
+from re import T
+
+
 f = open("myInputDay25.txt", "r")
 boardstate = []
 for line in f:
@@ -5,7 +8,28 @@ for line in f:
 print(boardstate)
 
 def canMoveEast(line, position):
-    if 
+    if line[position + 1] == ".":
+        return(True)
+
+def canMoveSouth(nextLine, position):
+    if nextLine[position] == ".":
+        return(True)
+
+#def canStep(line, position, nextLine, slug):
+#    if slug == ">":
+#        return(canMoveEast(line, position))
+#    if slug == "v":
+#        return(canMoveSouth(nextLine, position))
 
 def step(boardstate):
     for i in range(len(boardstate)):
+        for j in range(len(boardstate[i])):
+            #if can(boardstate[i], j, boardstate[i + 1], boardstate[i][j]):
+            if boardstate[i][j] == ">":
+                if canMoveEast(boardstate[i], j):
+                    boardstate[i][j] = "."
+                    boardstate[i][j + 1] = ">"
+            if boardstate[i][j] == "v":
+                if canMoveSouth(boardstate[i + 1], j):
+                    boardstate[i][j] = "."
+                    boardstate[i + 1][j] = "v"
