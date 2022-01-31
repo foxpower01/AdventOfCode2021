be78a226e76e8b6e575d8f4a09a2b0398f45600a
foxpower01 <rin.studys@gmail.com>
2022-01-31 09:20:19 -0800

    in progress still

--- adventDay25.py
+++ adventDay25.py
@@ -1,12 +1,12 @@
 numberOfSteps = 0
 hasStepped = True
-f = open("myInputDay25.txt", "r")
+f = open("test.txt", "r")
 boardstateIn = []
 for line in f:
     boardstateIn.append(list(filter(lambda a: a != "\n", line)))
 
 def canMoveEast(line, position):
-    if position + 2 > len(line):
+    if position + 1 == len(line):
         if line[0] == ".":
             return("0")
     else:
@@ -25,37 +25,49 @@ def canMoveSouth(nextLine, position):
 #        return(canMoveSouth(nextLine, position))
 
 def step(boardstate):
+    global hasStepped
     hasStepped = False
     for i in range(len(boardstate)):
         for j in range(len(boardstate[i])):
-            #if can(boardstate[i], j, boardstate[i + 1], boardstate[i][j]):
             if boardstate[i][j] == ">":
                 if canMoveEast(boardstate[i], j) == "0":
                     boardstate[i][j] = "."
                     boardstate[i][0] = "e"
                     hasStepped = True
-                    print("hi")
                 if canMoveEast(boardstate[i], j) == "pos":
                     boardstate[i][j] = "."
                     boardstate[i][j + 1] = "e"
                     hasStepped = True
-                    print("hi")
+                    print("moved East")
+    for i in range(len(boardstate)):
+        for j in range(len(boardstate[i])):
             if boardstate[i][j] == "v":
                 if i + 1 == len(boardstate):
                     if canMoveSouth(boardstate[0], j):
                         boardstate[i][j] = "."
                         boardstate[0][j] = "s"
                         hasStepped = True
-                        print("hi")
                 else :
                     if canMoveSouth(boardstate[i + 1], j):
                         boardstate[i][j] = "."
                         boardstate[i + 1][j] = "s"
                         hasStepped = True
-                        print("hi")
+                       # print("moved south")
+    for line in boardstate:
+        for k in range(len(line)):
+            if line[k] == "e":
+                line[k] = ">"
+            if line[k] == "s":
+                line[k] = "v"
+    #print(boardstate)
+    return(boardstate)
 
-while hasStepped == True:
+#while hasStepped == True:
+for i in range(59):
     numberOfSteps += 1
-    step(boardstateIn)
+    boardstateIn = step(boardstateIn)
+    print(boardstateIn)
     print(hasStepped)
 print(numberOfSteps)
+
+#print(step(boardstateIn))
